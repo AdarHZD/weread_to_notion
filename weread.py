24bd282eb12c8cc9bca70034cef5edb24b5b2cd7
AdarHZD <130363990+AdarHZD@users.noreply.github.com>
2023-06-16 00:36:08 +0800

    weread.py

--- weread.py
+++ weread.py
@@ -194,7 +194,7 @@ def get_chapter_info(bookId):
     return None
 
 
-def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating):
+def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating,relationtosum):
     """插入到notion"""
     time.sleep(0.3)
     parent = {
@@ -209,7 +209,7 @@ def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating):
         "Author": {"rich_text": [{"type": "text", "text": {"content": author}}]},
         "Sort": {"number": sort},
         "Rating": {"number": rating},
-        "信息处理统计": {"relation": [{"id": "0bc1af24b02b4775bb53cfa522251afa"}]},
+        "relationtosum": {"relation": [{"type": "database_id", "id": "0bc1af24b02b4775bb53cfa522251afa"}]},
         "Cover": {"files": [{"type": "external", "name": "Cover", "external": {"url": cover}}]},
     }
     read_info = get_read_info(bookId=bookId)
