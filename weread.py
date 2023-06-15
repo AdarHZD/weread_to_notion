7a1b66a92f352506fa1d08179ede0bfae2f047ae
AdarHZD <130363990+AdarHZD@users.noreply.github.com>
2023-06-16 01:14:05 +0800

    weread.py

--- weread.py
+++ weread.py
@@ -194,7 +194,7 @@ def get_chapter_info(bookId):
     return None
 
 
-def insert_to_notion(bookName, bookId, cover, sort, author, isbn, rating, relationtosum):
+def insert_to_notion(bookName, bookId, cover, sort, author, isbn, rating):
     """插入到notion"""
     time.sleep(0.3)
     parent = {
@@ -209,7 +209,6 @@ def insert_to_notion(bookName, bookId, cover, sort, author, isbn, rating, relati
         "Author": {"rich_text": [{"type": "text", "text": {"content": author}}]},
         "Sort": {"number": sort},
         "Rating": {"number": rating},
-        "relationtosum": relationtosum,
         "Cover": {"files": [{"type": "external", "name": "Cover", "external": {"url": cover}}]},
     }
     read_info = get_read_info(bookId=bookId)
@@ -410,8 +409,7 @@ if __name__ == "__main__":
             isbn,rating = get_bookinfo(bookId)
             children, grandchild = get_children(
                 chapter, summary, bookmark_list)
-            relationtosum = {"relation": [{"id": "0bc1af24b02b4775bb53cfa522251afa"}]},  
-            id = insert_to_notion(title, bookId, cover, sort, author, isbn, rating, relationtosum)
+            id = insert_to_notion(title, bookId, cover, sort, author, isbn, rating)
             results = add_children(id, children)
             if(len(grandchild)>0 and results!=None):
                 add_grandchild(grandchild, results)
